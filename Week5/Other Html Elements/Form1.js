b0d686897dbdafb13ec66a59beaf47e8ac3fa038
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-06-19 10:15:35 -0400

    Added a drop down menu

--- Week5/Other Html Elements/Form1.js
+++ Week5/Other Html Elements/Form1.js
@@ -1,11 +1,10 @@
+
+
 const buttonP = document.createElement('button');
 document.body.appendChild(buttonP);
 buttonP.textContent = "Proceed"
 buttonP.style.cssText = "margin:10px"
 
-
-
 buttonP.onclick = function(){
-    alert(document.getElementById('FirstName').value + " " + document.getElementById('LastName').value +  " " + document.getElementById('Age').value + " "   +  " " + document.getElementById('Toes') + " " + document.getElementsByName('Allergy').value);
+    alert(document.getElementById('FirstName').value + " " + document.getElementById('LastName').value +  " " + document.getElementById('Age').value + " "   +  " " + document.getElementById('Toes') + " " + document.getElementsByName('allergies').value);
 }
-console.log(document.getElementsByName('Toes').value);
