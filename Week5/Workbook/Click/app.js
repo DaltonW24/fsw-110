a2f8df0c1fd5ce0b616e8dce827985e2c4fad0b4
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-07-02 20:42:15 -0400

    Finished Workbook

---
+++ Week5/Workbook/Click/app.js
@@ -0,0 +1,4 @@
+// make the box disapear when the user clicks it
+document.getElementById('RedBox').addEventListener('click', function(){
+    document.getElementById('RedBox').style.cssText = "background-color: white"
+}) 
